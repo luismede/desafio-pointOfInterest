890e7fd25d82d517cbd78a2cbf0846e0f2350f9b
luismede <luishenrique23hh23@gmail.com>
2025-07-12 02:09:43 -0300

    feat: Create PointOfInterest Controller

---
+++ src/main/java/com/xygps/poi/controller/PointOfInterestController.java
@@ -0,0 +1,35 @@
+package com.xygps.poi.controller;
+
+import com.xygps.poi.dto.PointOfInterestDTO;
+import com.xygps.poi.model.PointOfInterest;
+import com.xygps.poi.service.PointOfInterestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class PointOfInterestController {
+    @Autowired
+    private PointOfInterestService service;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<PointOfInterest> createPoi(@RequestBody PointOfInterestDTO dto) {
+        PointOfInterest poi = service.createPoi(dto.transformToObject());
+        return new ResponseEntity<>(poi, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public List<PointOfInterestDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/location")
+    public List<PointOfInterestDTO> listAllByLocation(@RequestParam Long max_distance, Long position_x, Long position_y) {
+        return service.findAllByNearbyLocation(max_distance, position_x, position_y);
+    }
+}
