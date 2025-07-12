95c93e7b6e8363e2e55ef41aa74dcd7274917d00
luismede <luishenrique23hh23@gmail.com>
2025-07-12 02:10:15 -0300

    feat: Create PointOfInterest Repository

---
+++ src/main/java/com/xygps/poi/repository/PointOfInterestRepository.java
@@ -0,0 +1,20 @@
+package com.xygps.poi.repository;
+
+import com.xygps.poi.model.PointOfInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
+    @Query(value = "SELECT tb.id, tb.name, tb.coord_x, tb.coord_y " +
+            "FROM TB_POINT_OF_INTEREST tb " +
+            "WHERE SQRT(POWER(tb.coord_x - CAST(:#{#position_x} as double), 2) + " +
+            "POWER(tb.coord_y - CAST(:#{#position_y} as double), 2) ) <= CAST(:#{#dmax} as double)", nativeQuery = true)
+    List<PointOfInterest> findAllByNearbyLocation(@Param("dmax") Long dmax,
+                                                  @Param("position_x") Long position_x,
+                                                  @Param("position_y") Long position_y);
+}
