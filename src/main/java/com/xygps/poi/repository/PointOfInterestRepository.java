c6dedf7d13cef59760021606198ddeafefa5d027
luismede <luishenrique23hh23@gmail.com>
2025-07-14 18:18:39 -0300

    perf: improve performance of nearby search query in PointOfInterestRepository

--- src/main/java/com/xygps/poi/repository/PointOfInterestRepository.java
+++ src/main/java/com/xygps/poi/repository/PointOfInterestRepository.java
@@ -12,8 +12,9 @@ import java.util.List;
 public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
     @Query(value = "SELECT tb.id, tb.name, tb.coord_x, tb.coord_y " +
             "FROM TB_POINT_OF_INTEREST tb " +
-            "WHERE SQRT(POWER(tb.coord_x - CAST(:#{#position_x} as double), 2) + " +
-            "POWER(tb.coord_y - CAST(:#{#position_y} as double), 2) ) <= CAST(:#{#dmax} as double)", nativeQuery = true)
+            "WHERE " +
+            "  POWER(tb.coord_x - :position_x, 2) +" +
+            "  POWER(tb.coord_y - :position_y, 2) <= POWER(:dmax, 2)", nativeQuery = true)
     List<PointOfInterest> findAllByNearbyLocation(@Param("dmax") Long dmax,
                                                   @Param("position_x") Long position_x,
                                                   @Param("position_y") Long position_y);
